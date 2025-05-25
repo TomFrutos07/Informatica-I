6985bd111a276a8d54a4d14ba7a4d1f8d8c0bc9f
Tomás Frutos <tomasfrutos230715@gmail.com>
2025-05-25 00:33:09 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -14,7 +14,13 @@ int main() {
 
     printf("\nSu índice de masa corporal es: %.1f\n\n", imc);
 
-       printf("Condición: ");
+  printf("Condición:\n");
+    printf("<18.5 Bajo peso\n");
+    printf("18.5 a 24.9 Normal\n");
+    printf("25.0 a 29.9 Sobrepeso\n");
+    printf(">=30 Obesidad\n");
+
+       printf("Su condición: ");
     if (imc < 18.5) {
         printf("Bajo peso\n");
     } else if (imc < 25.0) {
